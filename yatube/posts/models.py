c2ad1ce438667f0c0516643df5ffe68975195890
Alexander K <a.kazarin@mail.ru>
2022-05-13 19:02:05 +0300

    'optimisation for hw_pre_test'

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,15 +4,15 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    group = models.ForeignKey('Group', blank=True, null=True, on_delete=models.CASCADE)
+    text = models.TextField(verbose_name= 'Текст')
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name= 'Дата публикации')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name= 'Автор')
+    group = models.ForeignKey('Group', blank=True, null=True, on_delete=models.CASCADE, verbose_name= 'Сообщество')
 
 class Group(models.Model):
     title = models.CharField(max_length=100, verbose_name='Название группы')
-    slug = models.SlugField(max_length=100, db_index=True, verbose_name= 'URL')
-    description = models.TextField()
+    slug = models.SlugField(max_length=100, db_index=True, verbose_name= 'Адрес')
+    description = models.TextField(verbose_name= 'Описание')
 
     def __str__(self):
         return self.title
